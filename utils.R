cbd86f6d26f95bf77fbe199e595256ecf12e1149
Dom G <21165092+dgrisafe@users.noreply.github.com>
2023-12-13 17:25:35 -0600

    Add post image code to footer of post, agitation post updated only

--- utils.R
+++ utils.R
@@ -1,5 +1,6 @@
 library(htmltools)
 
+# thumbnail images for multiple posts on home page
 thumbnail <- function(title, img, href, class = "col-sm-6", caption = TRUE, width = NULL) {
   div(class = class,
       a(class = "thumbnail", title = title, href = href,
@@ -7,5 +8,15 @@ thumbnail <- function(title, img, href, class = "col-sm-6", caption = TRUE, widt
         div(class = if (caption) "caption",
             if (caption) title)
       )
+  )
+}
+
+# post image, usually in footer of post
+post_image <- function(img_path, class = "col-sm-6"){
+  p(style = "clear:both;")
+  div(class = class, style="text-align: center;",
+      hr(),
+      htmltools::img(src = img_path, width="50%"),
+      hr()
   )
 }
